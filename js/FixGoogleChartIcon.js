62a88284510866ff55d2b8c89cb473814972f910
Shawn Nasiso Ray <shawn.ray.mac@gmail.com>
2013-02-28 17:58:01 +0800

    Remove the useless fix google chart icon;

--- js/FixGoogleChartIcon.js
+++ js/FixGoogleChartIcon.js
@@ -3,8 +3,4 @@
  * @author gary.gan
  */
 $(document).ready(function(){
-	$('.google-visualization-formatters-arrow-dr').each(function(){
-			$(this).attr('class', "google-visualization-formatters-arrow-dr");
-		}
-	);
 });
