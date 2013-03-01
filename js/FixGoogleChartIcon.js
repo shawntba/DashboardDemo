5bb7e4e046a586d6520961a8b6c1640f28e73b08
shawntba <shawn.ray.mac@gmail.com>
2013-03-01 16:06:26 +0800

    Add getYearDropdownHtml method;

--- js/FixGoogleChartIcon.js
+++ js/FixGoogleChartIcon.js
@@ -5,3 +5,18 @@
 $(document).ready(function(){
 	
 });
+
+/**
+ * Get the Year param selector html
+ * @param {string} yearSelectorId
+ */
+function getYearDropdownHtml(yearSelectorId){
+	var currentDate = new Date();
+
+	var optionHtmlString = '';
+	for (var i = currentDate.getFullYear(); i >= 2012; i--) {
+		optionHtmlString.append("<option value='" + i + "'>" + i + "</option>");
+	};
+
+	return "<select id='" + yearSelectorId + "'>" + optionHtmlString + "</select>";
+}
